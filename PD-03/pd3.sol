7af0e77bc9e7d18bdaf2040ac65df152fd9f61ee
Sarah <thie0014@hz.nl>
2021-02-19 17:49:13 +0100

    update

---
+++ PD-03/pd3.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma experimental ABIEncoderV2;
+pragma solidity 0.7.4;
+
+contract Class {
+
+    struct Student {
+        string name;
+        uint8 age;
+        bool exists;
+    }
+    
+    // add mapping
+    mapping (address => Student) students;
+    address[] private studentAccounts;
+    
+    // add event
+    event logAddStudent (
+    	uint indexed date,
+    	string indexed name,
+    	uint8 age,
+    	string message
+    );
+    
+        function setStudent(address _address, string memory _name, uint8 _age) public {
+        // add error handling
+        if (!students[_address].exists)
+        {
+            Student storage student = students[_address];
+            student.name = _name;
+            student.age = _age;
+            student.exists = true;
+            studentAccounts.push(_address);
+            // call event
+            emit logAddStudent(block.timestamp, _name, _age, "Test");
+        }
+        else
+        {
+            
+        }
+    }
+    
+    function getStudent(address _address) view public returns(Student memory){
+        return students[_address];
+    }
+  
+    function amountOfStudents() public view returns(uint count) {
+        return studentAccounts.length;
+    }
+
+}
